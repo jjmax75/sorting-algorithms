b532ba3d0d69fb809f38b037b8a339085b6b799d
John Behan <behanj@gmail.com>
2019-04-22 19:34:59 +0100

    refactor swap function

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -1,18 +1,16 @@
 // complexity worst case - O(Math.pow(n, 2))
 
-export const bubbleSort = arr => {
-	const swap = (i, j) => {
-		[arr[i], arr[j]] = [arr[j], arr[i]];
-	};
+const bubbleSort = arr => {
+	const swap = (i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]]);
 
-	const arrLastIndex = arr.length - 1;
+	const arrLength = arr.length;
 	let swapped = true;
 	let sortedCount = 0;
 
 	while (swapped) {
 		swapped = false;
 
-		for (let i = 0; i <= arrLastIndex - sortedCount; i++) {
+		for (let i = 0; i < arrLength - sortedCount; i++) {
 			if (arr[i] > arr[i + 1]) {
 				swap(i, i + 1);
 				swapped = true;
@@ -26,7 +24,6 @@ export const bubbleSort = arr => {
 };
 
 // test cases
-
 console.log(bubbleSort([5, 3, 1, 6, 8, 7, 2, 4]));
 console.log(bubbleSort([10, 99, 3, 56, 23, 11, 1, 12]));
 console.log(bubbleSort([900, -10, -88, 54, -123, -1, 7, 12, -666]));
